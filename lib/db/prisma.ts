d9b1362cf31a49e2c4dbc048917fc9173d2ba3eb
yoboypaste <57786066+yoboypaste@users.noreply.github.com>
2026-01-17 23:31:28 -0600

    Too many sessions fix

--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -1,11 +1,17 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
 
-const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-const db = new PrismaClient({ adapter: pool });
+// Prevent multiple instances of Prisma Client in development
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-const globalForPrisma = global as unknown as { prisma: typeof db }
+export const db = globalForPrisma.prisma || createPrismaClient()
+
+function createPrismaClient() {
+    // Only create the pool and client if we actually need one
+    const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+    return new PrismaClient({ adapter: pool })
+}
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
 
